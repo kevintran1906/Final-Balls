da4c9e06e8b543c119a58eefd32be3a8a60d6836
Kevin Tran <kevintran19062007@gmail.com>
2024-06-19 13:17:14 -0400

    Commit

--- main.py
+++ main.py
@@ -39,23 +39,23 @@ WIZARD_OFFSET = [112, 107]
 WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
 
 #load music and sounds
-pygame.mixer.music.load("assets\audio\assets_audio_music.mp3")
+pygame.mixer.music.load(r"assets\audio\music.mp3")
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1, 0.0, 5000)
-sword_fx = pygame.mixer.Sound("assets\audio\assets_audio_sword.wav")
+sword_fx = pygame.mixer.Sound(r"assets\audio\sword.wav")
 sword_fx.set_volume(0.5)
-magic_fx = pygame.mixer.Sound("assets\audio\assets_audio_magic.wav")
+magic_fx = pygame.mixer.Sound(r"assets\audio\magic.wav")
 magic_fx.set_volume(0.75)
 
 #load background image
-bg_image = pygame.image.load("assets\images\background\background.webp").convert_alpha()
+bg_image = pygame.image.load(r"assets\images\background\background.jpg").convert_alpha()
 
 #load spritesheets
-warrior_sheet = pygame.image.load("assets/images/warrior/Sprites/warrior.png").convert_alpha()
-wizard_sheet = pygame.image.load("assets/images/wizard/Sprites/wizard.png").convert_alpha()
+warrior_sheet = pygame.image.load(r"assets/images/warrior/Sprites/warrior.png").convert_alpha()
+wizard_sheet = pygame.image.load(r"assets/images/wizard/Sprites/wizard.png").convert_alpha()
 
 #load vicory image
-victory_img = pygame.image.load("assets\images\icons\victory2.png").convert_alpha()
+victory_img = pygame.image.load(r"assets\images\icons\victory.png").convert_alpha()
 
 #define number of steps in each animation
 WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
